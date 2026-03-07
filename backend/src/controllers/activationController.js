0d044d8f1c04e1409115148778c188aa879e3110
gabriellemosc <gabriel.palacios1910@gmail.com>
2026-03-07 19:11:52 -0300

    DEBUG

--- backend/src/controllers/activationController.js
+++ backend/src/controllers/activationController.js
@@ -8,10 +8,13 @@ const uploadActivation = async (req, res) => {
       if (!req.file) {
         return res.status(400).json({ error: 'Image Mandatory' })
       }
+
+      
   
       const promoterId = req.user.id
-  
+      console.log("Iniciando upload para o S3...");
       const s3Url = await uploadToS3(req.file)
+      console.log("Upload S3 concluído. URL:", s3Url);
   
       const query = `
         INSERT INTO photos (s3_url, promoter_id)
@@ -24,8 +27,13 @@ const uploadActivation = async (req, res) => {
       return res.status(201).json(result.rows[0])
   
     } catch (error) {
-      console.error(error)
-      return res.status(500).json({ error: 'Error to Save Image' })
+      // AQUI ESTÁ A MUDANÇA: Retorna o erro real
+      console.error("ERRO DETALHADO:", error);
+      return res.status(500).json({ 
+        error: 'Error to Save Image', 
+        details: error.message, // Isso mostrará o erro real no navegador
+        stack: error.stack 
+      })
     }
   }
   
