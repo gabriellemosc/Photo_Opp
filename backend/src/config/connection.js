ce69b45751527f28be6c029f0ed9b42ec677edce
gabriellemosc <gabriel.palacios1910@gmail.com>
2026-03-07 13:38:59 -0300

    Fix Connection

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,12 +1,20 @@
-require('dotenv').config()
-const { Pool } = require('pg') 
+// connection.js
+const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.ACTIVACAO_DB_HOST,        
-  user: process.env.ACTIVACAO_DB_USER,        
-  password: process.env.ACTIVACAO_DB_PASSWORD,
-  database: process.env.ACTIVACAO_DB_NAME,    
-  port: process.env.ACTIVACAO_DB_PORT         
-})
+let pool;
 
-module.exports = pool
+// Evita criar múltiplas pools em serverless (Vercel)
+if (!global.ACTIVACAO_POOL) {
+  pool = new Pool({
+    host: process.env.ACTIVACAO_DB_HOST,
+    user: process.env.ACTIVACAO_DB_USER,
+    password: process.env.ACTIVACAO_DB_PASSWORD,
+    database: process.env.ACTIVACAO_DB_NAME,
+    port: process.env.ACTIVACAO_DB_PORT
+  });
+  global.ACTIVACAO_POOL = pool;
+} else {
+  pool = global.ACTIVACAO_POOL;
+}
+
+module.exports = pool;
